4fb6a0450704b8908b89cd57f2b44ac099615ae6
Robert Ang <angrobertsh@gmail.com>
2017-04-27 19:32:38 -0700

    forgot to put actions on window

--- frontend/sffoodtrucks.jsx
+++ frontend/sffoodtrucks.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-// import * as ACTIONS from './actions/truck_actions';
+import * as ACTIONS from './actions/truck_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
